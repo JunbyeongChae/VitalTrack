35342a1772ad788085d550491d589cec9c1afaf5
khesed6537 <mindstate82@gmail.com>
2025-03-13 02:13:58 +0900

    검색모달 기능추가, UI 업데이트

--- front_end/src/components/include/Header.jsx
+++ front_end/src/components/include/Header.jsx
@@ -49,7 +49,7 @@ const Header = ({ user, setUser }) => {
               <button onClick={() => navigate('/healthInfo')} className="border-b-2 border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium">
                 건강정보
               </button>
-              <button onClick={() => navigate('/work')} className="border-transparent text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+              <button onClick={() => navigate('/workout')} className="border-transparent text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                 운동관리
               </button>
               <button onClick={() => navigate('/nutrition')} className="border-transparent text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
