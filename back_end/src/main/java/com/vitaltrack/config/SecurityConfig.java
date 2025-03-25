c7cf3b80c13f77dc59151bd190f0ba890faa5ed4
JunbyeongChae <wnsqud4545@gmail.com>
2025-03-25 13:32:25 +0900

    .render.yaml생성

--- back_end/src/main/java/com/vitaltrack/config/SecurityConfig.java
+++ back_end/src/main/java/com/vitaltrack/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.vitaltrack.config;
 import com.vitaltrack.util.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -26,6 +27,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .requestMatchers("/api/auth/**","/api/counsel/**","/api/infoboard/**","/api/workout/**").permitAll()
             .anyRequest().authenticated())
         .addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
