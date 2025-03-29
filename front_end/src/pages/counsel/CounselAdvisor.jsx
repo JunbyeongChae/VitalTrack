5c8bd044ff8a543ee2e935949aad9fc9346a19b9
JunbyeongChae <wnsqud4545@gmail.com>
2025-03-29 17:07:05 +0900

    advisor 수정

--- front_end/src/pages/counsel/CounselAdvisor.jsx
+++ front_end/src/pages/counsel/CounselAdvisor.jsx
@@ -13,18 +13,14 @@ const CounselAdvisor = () => {
           {/* 수정 내용: 사이드바 반응형 처리 */}
           <CounselSidebar />
         </div>
-        <div className="w-full md:w-3/4 md:mt-0 md:ml-6 p-6 bg-[#f2f5eb] text-[#5f7a60] rounded-xl shadow-lg border border-[#c2c8b0]">
-          {' '}
-          {/* 수정 내용: 본문 패널 반응형 처리 */}
+        <div className="w-full md:w-3/4 mt-6 md:mt-0 md:ml-6 p-6 bg-[#f2f5eb] text-[#5f7a60] rounded-xl shadow-lg border border-[#c2c8b0]">
           <div className="flex justify-between items-center border-b pb-4 mb-4 border-[#c2c8b0]">
             <h1 className="text-2xl font-semibold text-[#7c9473]">상담사 소개</h1>
           </div>
-          <div className="flex justify-center mt-1 flex-wrap gap-1 pb-2">
-            <img
-              src="/images/advisor.png"
-              alt="영양사 면허"
-              className="rounded-lg shadow-md w-full max-w-[400px] mx-auto" // ✅ PC에서 max-width 제한, 중앙정렬
-            />
+
+          {/* ✅ 이미지 정렬 및 크기 수정 */}
+          <div className="flex justify-center mt-1 pb-2">
+            <img src="/images/advisor.png" alt="영양사 면허" className="rounded-lg shadow-md max-w-[500px] mx-auto" />
           </div>
         </div>
       </div>
